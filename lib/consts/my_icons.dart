b59fe039554929d43fae6016e36707ea5adbed00
Sadid369 <sadid.jones@gmail.com>
2021-12-03 17:03:40 +0600

    002 Using custom icons

---
+++ lib/consts/my_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyAppIcons {
+  static IconData home = Icons.home;
+  static IconData rss = Icons.rss_feed;
+  static IconData cart = Icons.shopping_cart;
+  static IconData user = Icons.person;
+  //static IconData search = Icons.scarch;
+}
